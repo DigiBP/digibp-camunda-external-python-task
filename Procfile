f0e2bba93dce5a2d18834619386230c093b14416
Andreas Martin <andreas.martin@andreasmartin.ch>
2020-02-26 11:33:23 +0100

    Update for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python SurpriseMenuClient.py
+web: gunicorn app:app
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to Camunda Python Client"
+
+
+if __name__ == '__main__':
+    subprocess.Popen("SurpriseMenuClient.py", shell=True)
+    app.run()
