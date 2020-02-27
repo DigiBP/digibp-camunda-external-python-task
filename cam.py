c727c872fb3291e1e17eabf8a381c0ebb4554026
Andreas Martin <andreas.martin@andreasmartin.ch>
2020-02-27 15:53:59 +0100

    Refactoring

--- cam.py
+++ cam.py
@@ -8,10 +8,13 @@ import uuid
 class Client:
     def __init__(self, url, workerid=None):
         self.url = url
-        self.workerid = workerid
+        if workerid:
+            self.workerid = str(workerid)
+        else:
+            self.workerid = str(uuid.uuid1())
         self.threads = []
 
-    def __fetch_and_lock(self, endpoint, task, callback, interval=300):
+    def __fetch_and_lock(self, endpoint, task, callback=None, interval=300):
         body = '[]'
 
         try:
@@ -21,27 +24,22 @@ class Client:
                 print(fetch_and_lock.status_code)
                 body = fetch_and_lock.text
                 if body != '[]':
-                    callback(json.loads(body))
+                    if callback:
+                        callback(json.loads(body))
+                    else:
+                        return json.loads(body)
                 else:
                     time.sleep(interval / 1000)
 
         except:
             print("Engine is down")
 
-    def subscribe(self, topic, callback, tenantId=None, lockDuration=20000, longPolling=29000):
+    def subscribe(self, topic, callback=None, tenantId=None, lockDuration=20000, longPolling=29000):
         # Define the endpoint for fetch and lock
         endpoint = str(self.url) + "/external-task/fetchAndLock"
 
-        # Define unique ID for the worker, if undifiend
-        if self.workerid:
-            workerid = str(self.workerid)
-        else:
-            global uid
-            uid = uuid.uuid1()
-            workerid = str(uid)
-
         if tenantId:
-            task = {"workerId": workerid,
+            task = {"workerId": self.workerid,
                     "maxTasks": 1,
                     "usePriority": "true",
                     "asyncResponseTimeout": longPolling,
@@ -54,7 +52,7 @@ class Client:
                           }]
                     }
         else:
-            task = {"workerId": workerid,
+            task = {"workerId": self.workerid,
                     "maxTasks": 1,
                     "usePriority": "true",
                     "asyncResponseTimeout": longPolling,
@@ -63,8 +61,10 @@ class Client:
                           "lockDuration": lockDuration
                           }]
                     }
-
-        self.threads.append(threading.Thread(target=self.__fetch_and_lock, args=(endpoint, task, callback,)))
+        if callback:
+            self.threads.append(threading.Thread(target=self.__fetch_and_lock, args=(endpoint, task, callback,)))
+        else:
+            return self.__fetch_and_lock(endpoint, task)
 
     def polling(self):
         for thread in self.threads:
@@ -74,14 +74,9 @@ class Client:
 
     # Complete Call
     def complete(self, response_body, **kwargs):
-        taskid = response_body[0]['id']
-        taskid = str(taskid)
+        taskid = str(response_body[0]['id'])
 
         endpoint = str(self.url) + "/external-task/" + taskid + "/complete"
-
-        # get workerid
-        workerid = response_body[0]['workerId']
-        workerid = str(workerid)
 
         # puts the variables from the dictonary into the nested format for the json response
         variables_for_response = {}
@@ -89,7 +84,7 @@ class Client:
             variable_new = {key: {"value": val}}
             variables_for_response.update(variable_new)
 
-        response = {"workerId": workerid,
+        response = {"workerId": self.workerid,
                     "variables": variables_for_response
                     }
 
@@ -104,13 +99,9 @@ class Client:
 
     # BPMN Error
     def error(self, bpmn_error, response_body, error_message="not defined", **kwargs):
-        taskid = response_body[0]['id']
-        taskid = str(taskid)
+        taskid = str(response_body[0]['id'])
 
         endpoint = str(self.url) + "/external-task/" + taskid + "/bpmnError"
-
-        workerid = response_body[0]['workerId']
-        workerid = str(workerid)
 
         variables_for_response = {}
         for key, val in kwargs.items():
@@ -118,7 +109,7 @@ class Client:
             variables_for_response.update(variable_new)
 
         response = {
-            "workerId": workerid,
+            "workerId": self.workerid,
             "errorCode": bpmn_error,
             "errorMessage": error_message,
             "variables": variables_for_response
@@ -133,16 +124,12 @@ class Client:
 
     # Create an incident
     def fail(self, error_message, response_body, retries=0, retry_timeout=0):
-        taskid = response_body[0]['id']
-        taskid = str(taskid)
+        taskid = str(response_body[0]['id'])
 
         endpoint = str(self.url) + "/external-task/" + taskid + "/failure"
 
-        workerid = response_body[0]['workerId']
-        workerid = str(workerid)
-
         response = {
-            "workerId": workerid,
+            "workerId": self.workerid,
             "errorMessage": error_message,
             "retries": retries,
             "retryTimeout": retry_timeout}
@@ -151,5 +138,23 @@ class Client:
             fail = requests.post(endpoint, json=response)
             print(fail.status_code)
 
+        except:
+            print('fail')
+
+    # New Lockduration
+    def new_lockduration(self, new_duration, response_body):
+        taskid = str(response_body[0]['id'])
+
+        endpoint = str(self.url) + "/external-task/" + taskid + "/extendLock"
+
+        response = {
+            "workerId": self.workerid,
+            "newDuration": new_duration
+        }
+
+        try:
+            newDuration = requests.post(endpoint, json=response)
+            print(newDuration.status_code)
+            print(self.workerid)
         except:
             print('fail')
